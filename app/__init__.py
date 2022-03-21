ea55b37f81b0c0086ee2d1bfcffc2737a431f9f1
Abbi Parchment <abbigail151@gmail.com>
2022-03-21 12:30:37 -0500

    Completed Project

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,5 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 from app import views  
+
+from app.models import Prop
--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = 'postgres://sfnpwqxthvcljn:a3f09150eb359e05bb9c277a6dacf441f86fb6d57d974997a3d354ac7a3d6235@ec2-52-22-226-8.compute-1.amazonaws.com:5432/dahlo59pomd91f'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER= './uploads'
     
